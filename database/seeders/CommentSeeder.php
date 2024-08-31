2dcd9d6e182c0355ec5887eeb141d61a4fabba0f
OwakuHikaru <OwakuHikaru@gmail.com>
2024-08-31 19:45:00 +0900

    コメントシーダー削除

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -12,23 +12,23 @@ class CommentSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        DB::table('comments')->insert([
-            'user_id'=>1,
-            'post_id'=>1,
-            'comments'=>'おなかすいた',
-            'created_at' => new DateTime(),
-            'updated_at' => new DateTime(),
-            'deleted_at' => Null,
-            ]);
-        DB::table('comments')->insert([
-            'user_id'=>2,
-            'post_id'=>1,
-            'comments'=>'元気だして',
-            'created_at' => new DateTime(),
-            'updated_at' => new DateTime(),
-            'deleted_at' => Null,
-            ]);
-    }
+    // public function run(): void
+    // {
+    //     DB::table('comments')->insert([
+    //         'user_id'=>1,
+    //         'post_id'=>1,
+    //         'comments'=>'おなかすいた',
+    //         'created_at' => new DateTime(),
+    //         'updated_at' => new DateTime(),
+    //         'deleted_at' => Null,
+    //         ]);
+    //     DB::table('comments')->insert([
+    //         'user_id'=>2,
+    //         'post_id'=>1,
+    //         'comments'=>'元気だして',
+    //         'created_at' => new DateTime(),
+    //         'updated_at' => new DateTime(),
+    //         'deleted_at' => Null,
+    //         ]);
+    // }
 }
