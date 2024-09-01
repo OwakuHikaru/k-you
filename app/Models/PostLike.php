07c8e871bc3c045e308697421535e0a464a7e5a2
OwakuHikaru <OwakuHikaru@gmail.com>
2024-09-01 18:46:45 +0900

    いいね機能の修正

--- app/Models/PostLike.php
+++ app/Models/PostLike.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostLike extends Model
 {
+    use HasFactory;
     protected $primaryKey = ['user_id', 'post_id'];
     protected $fillable = ['post_id', 'user_id'];
     public $incrementing = false;
-    use HasFactory;
 }
