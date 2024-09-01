4b3555c08dd96636e2ea11173dbe01f3710d756c
OwakuHikaru <owakuhikaru@gmail.com>
2024-09-01 15:08:55 +0900

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -110,26 +110,21 @@
             //記事のidを取得しています。
             const postId = e.target.id
             //fetchメソッドを利用し、バックエンドと通信します。非同期処理のため、画面がかくついたり、真っ白になることはありません。
-            const res = await fetch('/post/like',{
-                //リクエストメソッドはPOST
-                method: 'POST',
+            axios.post("/like", {
+                post_id: postId,
+            }, {
                 headers: {
-                    //Content-Typeでサーバーに送るデータの種類を伝える。今回はapplication/json
-                    'Content-Type': 'application/json',
-                    //csrfトークンを付与
-                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                },
-                //バックエンドにいいねをした記事のidを送信します。
-                body: JSON.stringify({ post_id: postId })
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": document.querySelector('meta[name="csrf-token"]').getAttribute("content")
+                }
             })
-            .then((res)=>res.json())
-            .then((data)=>{
-                //記事のいいね数がバックエンドからlikesCountという変数に格納されて送信されるため、それを受け取りビューに反映します。
-                clickedEl.nextElementSibling.innerHTML = data.likesCount;
+            .then((response) => {
+                // サーバーからのレスポンスデータを使用
+                clickedEl.nextElementSibling.innerHTML = response.data.likesCount;
             })
-            .catch(
-            //処理がなんらかの理由で失敗した場合に実施したい処理を記述します。
-            ()=>alert('処理が失敗しました。画面を再読み込みし、通信環境の良い場所で再度お試しください。'))
+            .catch(() => {
+                alert("処理が失敗しました。画面を再読み込みし、通信環境の良い場所で再度お試しください。");
+            });
             })
             });
             </script>
